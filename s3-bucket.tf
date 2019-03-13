016269b1293df233d0a429241bf1d698140c1942
Marko Bajic <marko.bajic@mbajic-osx.local>
2019-03-12 21:53:39 -0400

    vpc

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,18 @@
 provider "aws" {}
 
-resource "aws_s3_bucket" "example" {
-  bucket = "cb-code-deploy-bucket-sate-test"
-  acl    = "private"
+terraform {
+  backend "s3" {
+    bucket = "cb-code-deploy-bucket-sate-tes"
+    key    = "state"
+    region = "us-east-2"
+  }
+}
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "dedicated"
+
+  tags = {
+    Name = "main"
+  }
 }
