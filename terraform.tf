35ed56bc98c9a7ba538a573500b738e8de1019d7
Marko Bajic <marko.bajic@mbajic-osx.cbmain.clarabridge.com>
2019-03-13 15:46:04 -0400

    s3

--- terraform.tf
+++ terraform.tf
@@ -2,7 +2,7 @@ provider "aws" {}
 
 terraform {
   backend "s3" {
-    bucket = "b-code-deploy-bucket-sate-test"
+    bucket = "cb-code-deploy-bucket-sate-test"
     key    = "state"
     region = "us-east-2"
   }
